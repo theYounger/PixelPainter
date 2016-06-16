849a6f8b5a34068bc22d161a6157b1b7fca54693
theYounger <kuyehara1@gmail.com>
2016-06-16 12:58:12 -1000

    testing phase 001

--- js/PixelPainter.js
+++ js/PixelPainter.js
@@ -1,20 +1,11 @@
+var pixelPainter = document.getElementById('pixelPainter');
+var ppCanvas = document.createElement('div');
+pixelPainter.appendChild(ppCanvas);
 
-function PixelPainter() {
-  var pixelPainter = document.getElementById('pixelPainter');
-  var ppCanvas = document.createElement('div');
 
-  var blockContainer = document.createElement('section');
-  blockContainer.setAttribute('class', 'blocks');
-  ppCanvas.appendChild(blockContainer);
-  pixelPainter.appendChild(ppCanvas);
-
-  for (i = 0; i < 400; i++) {
-    var newBlock = document.createElement('div');
-    newBlock.setAttribute('class', 'block');
-    newBlock.setAttribute('id', 'block' + i);
-    blockContainer.append(newBlock);
-  }
+for (i = 0; i < 400; i++) {
+  var newBlock = document.createElement('div');
+  newBlock.setAttribute('class', 'block');
+  newBlock.setAttribute('id', 'block' + i);
+  blockContainer.append(newBlock);
 }
-
-PixelPainter();
-
