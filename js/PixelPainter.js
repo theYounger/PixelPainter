2a78fdcb1983619ab9d8546be6606e697eaf8d84
Tyler Chong <tylrchong@gmail.com>
2016-06-16 12:10:30 -1000

    here

--- js/PixelPainter.js
+++ js/PixelPainter.js
@@ -1,21 +1,20 @@
 
-function PixelPainter(width, height) {
+function PixelPainter() {
   var pixelPainter = document.getElementById('pixelPainter');
   var ppCanvas = document.createElement('div');
-  pixelPainter.appendChild(ppCanvas);
 
   var blockContainer = document.createElement('section');
   blockContainer.setAttribute('class', 'blocks');
   ppCanvas.appendChild(blockContainer);
+  pixelPainter.appendChild(ppCanvas);
 
-  $blockContainer = $(".blocks");
   for (i = 0; i < 400; i++) {
     var newBlock = document.createElement('div');
     newBlock.setAttribute('class', 'block');
     newBlock.setAttribute('id', 'block' + i);
-    $blockContainer.append(newBlock);
+    blockContainer.append(newBlock);
   }
 }
 
-console.log('hi tyler');
+PixelPainter();
 
