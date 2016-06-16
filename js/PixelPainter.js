624374276657bc27099807ea0a5dbf4852a9cdb5
Tyler Chong <tylrchong@gmail.com>
2016-06-16 12:57:24 -1000

    lol

--- js/PixelPainter.js
+++ js/PixelPainter.js
@@ -1,20 +1,18 @@
-
 function PixelPainter() {
   var pixelPainter = document.getElementById('pixelPainter');
-  var ppCanvas = document.createElement('div');
 
-  var blockContainer = document.createElement('section');
+  var blockContainer = document.createElement('div');
   blockContainer.setAttribute('class', 'blocks');
-  ppCanvas.appendChild(blockContainer);
-  pixelPainter.appendChild(ppCanvas);
 
-  for (i = 0; i < 400; i++) {
+  pixelPainter.appendChild(blockContainer);
+
+  for (var i = 0; i < 400; i++) {
     var newBlock = document.createElement('div');
     newBlock.setAttribute('class', 'block');
     newBlock.setAttribute('id', 'block' + i);
-    blockContainer.append(newBlock);
+    blockContainer.appendChild(newBlock);
   }
 }
 
-PixelPainter();
 
+PixelPainter();
