60f5138b73df43b8de98c16f138b5b08e7fd9da6
Tyler Chong <tylrchong@gmail.com>
2016-06-16 16:16:56 -1000

    ayylmao

--- js/PixelPainter.js
+++ js/PixelPainter.js
@@ -1,18 +1,25 @@
 function PixelPainter() {
   var pixelPainter = document.getElementById('pixelPainter');
+  var canvas = document.createElement('div');
+  canvas.id = 'canvas';
+  pixelPainter.appendChild(canvas);
 
   var blockContainer = document.createElement('div');
   blockContainer.setAttribute('class', 'blocks');
-
-  pixelPainter.appendChild(blockContainer);
+  canvas.appendChild(blockContainer);
 
   for (var i = 0; i < 400; i++) {
     var newBlock = document.createElement('div');
     newBlock.setAttribute('class', 'block');
     newBlock.setAttribute('id', 'block' + i);
+    newBlock.addEventListener('click', colorBlock);
     blockContainer.appendChild(newBlock);
   }
 }
 
+function colorBlock() {
+  console.log(this);
+  this.style.backgroundColor = 'red';
+}
 
 PixelPainter();
